8a816cf93c9bfd2f23dda743df9cd6942f9fd74f
Bill Lawrence <Bill.Lawrence@sterling.ng>
2024-04-06 09:32:14 +0100

    restructuring

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,28 +1,13 @@
-import { FuelProvider } from '@fuels/react';
-import {
-  FuelWalletConnector,
-  FuelWalletDevelopmentConnector,
-  FueletWalletConnector,
-} from '@fuels/connectors';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const queryClient = new QueryClient();
-
-const Index: React.FC = () => (
-  <QueryClientProvider client={queryClient}>
-    <FuelProvider
-      fuelConfig={{
-        connectors: [
-          new FuelWalletConnector(),
-          new FuelWalletDevelopmentConnector(),
-          new FueletWalletConnector(),
-        ],
-      }}
-    >
-      <App />
-    </FuelProvider>
-  </QueryClientProvider>
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
 );
-
-export default Index;
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
